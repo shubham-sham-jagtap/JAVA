ca1d16c75ba9e2b7c3dba7f28f6c84e353ce0164
shubham <sj0622431@gmail.com>
2024-09-22 18:48:57 +0530

    done with addition without using function

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Q. take input of 2 numbers and print the sum
-        
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter 1st Number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter 2nd Number: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum of the numbers are : " + sum);
     }
 }
