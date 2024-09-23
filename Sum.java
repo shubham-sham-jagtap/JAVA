b1259308b0b577f14d7da0b931de9a0fb8e1c5b6
shubham <sj0622431@gmail.com>
2024-09-24 00:55:13 +0530

    created a function using datatype and return type

--- Sum.java
+++ Sum.java
@@ -2,8 +2,22 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        sum();
+        int ans = sum2();
+        System.out.println(ans);
     } 
+
+
+    static int sum2() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int num1 = in.nextInt();
+
+        System.out.println("Enter 2nd number: ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+        return sum;
+    }
 
     static void sum() {
         Scanner in = new Scanner(System.in);
