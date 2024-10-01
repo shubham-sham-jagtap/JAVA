9e017b51a76a5793090295d6519d6239d15b3908
shubham <sj0622431@gmail.com>
2024-10-01 19:13:09 +0530

    done with shadowing part

--- Scope.java
+++ Scope.java
@@ -3,17 +3,16 @@
 //  in Java, all identifiers are lexically (or statically) scoped, i.e.scope of a variable can be 
 //  determined at compile time and independent of function call stack. 
 // Java programs are organized in the form of classes. Every class is part of some package.
-//  Java scope rules can be covered under following categories.
 
 public class Scope {
     public static void main(String[] args) {
-        int a = 20;
+        int a = 20; //initialised outside the block
         int b = 30;
 
         {
             // int a = 70;  -  already initialised outside the block in the same method, hence you cannot initialise again
             // but you can change the value
-            a = 100
+            a = 100 // update value
             int c = 50;
             // values initialised in this block, will remain in the block
          }
--- Shadowing.java
+++ Shadowing.java
@@ -1,3 +1,14 @@
 public class Shadowing {
-    
+    static int x = 20; // this will be shadowed at line 6
+    public static void main(String[] args) {
+        System.out.println(x); // 20
+        int x;
+        // Ayatem.out.println(x);  -  scope will begin when vale is initialised][\[]]
+        x = 80;
+        System.out.println(x); // 80
+        fun();
+    }
+    static void fun() {
+        System.out.println(x); //20
+    }
 }
