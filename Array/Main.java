7ba10f4052e97550b981796dbd696f78c45ffd97
shubham <sj0622431@gmail.com>
2024-10-05 18:14:26 +0530

    Started with array

---
+++ Array/Main.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Main {
+    public static void main(String[] args) {
+        //SYNTAX
+        // datatype[] variable_name = new datatype[]
+        int[] ros; //decleration of array. ros is getting defined in the stack
+        ros = new int[5]; // initialisation: actually here object is being created in the memory (heap)
+           
+     }
+}
