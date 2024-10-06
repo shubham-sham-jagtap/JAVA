50232730835da9a45863fe41ebcc09ef3762ec86
shubham <sj0622431@gmail.com>
2024-10-06 13:13:45 +0530

    completed concept of integer array

--- Array/Main.java
+++ Array/Main.java
@@ -1,11 +1,28 @@
 package Array;
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //SYNTAX
         // datatype[] variable_name = new datatype[]
-        int[] ros; //decleration of array. ros is getting defined in the stack
-        ros = new int[5]; // initialisation: actually here object is being created in the memory (heap)
+        // int[] ros; //decleration of array. ros is getting defined in the stack
+        // ros = new int[5]; // initialisation: actually here object is being created in the memory (heap)
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+        //input using for loop
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+        
+        // for(int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
+
+        // for(int num : arr) {
+        //     System.out.print(num);
+        // }
            
      }
 }
