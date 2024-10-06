509a06328fa4057898e7668b4c7eb1a6149e613e
shubham <jagtapshubhamsham@gmail.com>
2024-10-07 02:14:04 +0530

    done with PassinginFunctions.java

--- Array/PassinginFunctions.java
+++ Array/PassinginFunctions.java
@@ -1,5 +1,14 @@
 package Array;
-
+import java.util.Arrays;
 public class PassinginFunctions {
-    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 6, 9};
+        System.out.println(Arrays.toString(arr));
+        change(arr);
+    }
+
+    static void change(int[] num) {
+        num[1] = 100;
+        System.out.println(Arrays.toString(num));
+    }
 }
