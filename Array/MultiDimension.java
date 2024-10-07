3b1fb77477d834cb5a74009847a9a211f5c72411
shubham <jagtapshubhamsham@gmail.com>
2024-10-07 19:51:26 +0530

    updated and done with array

--- Array/MultiDimension.java
+++ Array/MultiDimension.java
@@ -1,4 +1,6 @@
 package Array;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MultiDimension {
@@ -30,12 +32,20 @@ public class MultiDimension {
          }
 
          //Output
-         for(int row = 0; row < arr.length; row++) {
-                // for each col in every row
-                for(int col = 0; col < arr[row].length; col++) {
-                    System.out.print(arr[row][col] + " ");
-                }
-                System.out.println();
-         }
+        //  for(int row = 0; row < arr.length; row++) {
+                // // for each col in every row
+                // for(int col = 0; col < arr[row].length; col++) {
+                //     System.out.print(arr[row][col] + " ");
+                // }
+                // System.out.println();
+        //  }
+
+        // for(int row = 0; row < arr.length; row++) {
+        //     System.out.println(Arrays.toString(arr[row]));
+        // }
+
+        for(int[] a : arr) {
+            System.out.println(Arrays.toString(a));
+        }
     }
 }
