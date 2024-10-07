e4154c6b936af45cac896f50250b8678d1c2e7d6
shubham <jagtapshubhamsham@gmail.com>
2024-10-07 19:35:27 +0530

    taking user input to print 2D array

--- Array/MultiDimension.java
+++ Array/MultiDimension.java
@@ -1,4 +1,5 @@
 package Array;
+import java.util.Scanner;
 
 public class MultiDimension {
     public static void main( String[] args) {
@@ -7,20 +8,34 @@ public class MultiDimension {
             * 4 5 6
             * 7 8 9
          */
+        Scanner in = new Scanner(System.in);
+        //  int[][] arr = new int[3][]; //there is no compulsion to specify the number of columns
 
-        //  int[][] arr = new int[3][3]; //there is no compulsion to specify the number of columns
+        //  int[][] arr = {
+        //     {1, 2, 3}, // 0th index
+        //     {4, 5, 9}, // 1st index
+        //     {6, 7, 8, 9} // 2nd index -> arr[2] = {6, 7, 8, 9}
+        //  };
 
-         int[][] arr = {
-            {1, 2, 3}, // 0th index
-            {4, 5, 9}, // 1st index
-            {6, 7, 8} // 2nd index -> arr[2] = {6, 7, 8, 9}
-         };
-
-        System.out.println(arr.length); //no of rows
+        // System.out.println(arr.length); //no of rows
          // input
 
-        //  for(i = 0; i < arr.length; i++) {
+         int[][] arr = new int[3][3];
 
-        //  }
+         for(int row = 0; row < arr.length; row++) {
+                // for each col in every row
+                for(int col = 0; col < arr[row].length; col++) {
+                    arr[row][col] = in.nextInt();
+                }
+         }
+
+         //Output
+         for(int row = 0; row < arr.length; row++) {
+                // for each col in every row
+                for(int col = 0; col < arr[row].length; col++) {
+                    System.out.print(arr[row][col] + " ");
+                }
+                System.out.println();
+         }
     }
 }
