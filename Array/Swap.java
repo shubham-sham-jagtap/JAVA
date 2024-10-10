ad018ad24da747197c5975f86306a2d6543373c1
shubham <jagtapshubhamsham@gmail.com>
2024-10-11 04:12:39 +0530

    reverse an array

--- Array/Swap.java
+++ Array/Swap.java
@@ -3,8 +3,20 @@ import java.util.Arrays;
 public class Swap {
 public static void main(String[] args) {
     int[] arr = {1, 3, 5, 6, 8, 9};
-    swap(arr, 1, 2);
+    // swap(arr, 1, 2);
+    reverse(arr);
     System.out.println(Arrays.toString(arr));
+}
+
+static void reverse(int[] arr) {
+    int start = 0;
+    int end = arr.length - 1;
+
+    while (start < end) {
+        swap(arr, start, end);
+        start++;
+        end--;
+    }
 }
 
 static void swap(int[] arr, int index1, int index2) {
