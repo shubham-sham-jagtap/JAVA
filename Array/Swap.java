db60918edfd67d37b616eadb1153a46785ea97ba
shubham <jagtapshubhamsham@gmail.com>
2024-10-12 23:30:59 +0530

    swap done

--- Array/Swap.java
+++ Array/Swap.java
@@ -8,6 +8,12 @@ public static void main(String[] args) {
     System.out.println(Arrays.toString(arr));
 }
 
+static void swap(int[] arr, int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
+
 static void reverse(int[] arr) {
     int start = 0;
     int end = arr.length - 1;
@@ -19,10 +25,5 @@ static void reverse(int[] arr) {
     }
 }
 
-static void swap(int[] arr, int index1, int index2) {
-    int temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2] = temp;
-}
 
 }
