614abe0bbae3d318514e6a3bb8a426629753ac57
shubham <jagtapshubhamsham@gmail.com>
2024-10-14 02:28:46 +0530

    Linear Search

---
+++ Linear Search/Code/Main.java
@@ -0,0 +1,14 @@
+package Linear Search.Code;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    //Search in the array: return the index if item found
+    //other wise if item not found return -1
+    static LinearSearch(int[] arr, int target) {
+        
+    }
+
+}
