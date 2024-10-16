88bce78df6a728c6a444b47ab19e1748591f848b
shubham <jagtapshubhamsham@gmail.com>
2024-10-16 10:52:46 +0530

    find even number of digits in an array

---
+++ Linear_Search/Code/EvenDigits.java
@@ -0,0 +1,33 @@
+package Linear_Search.Code;
+
+public class EvenDigits {
+    public static void main(String[] args) {
+        //Find even number of digits in an array
+        int[] arr = {1, 2, 12, 23, 123, 453, 4567, 12254};
+        int ans = FindNumbers(arr);
+        System.out.println(ans);
+    }
+    static int FindNumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums) {
+            if(even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static boolean even(int num) {
+        int numberOfDigits = digit(num);
+        return numberOfDigits % 2 == 0;
+    }
+
+    static int digit(int num) {
+        int count = 0;
+        while(num > 0) {
+            count++;
+            num = num / 10; // num /= 10
+        }
+        return count;
+    }
+}
