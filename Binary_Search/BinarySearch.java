3bb78eea2cb571cb03f37333c51e408792094dde
shubham <jagtapshubhamsham@gmail.com>
2024-10-17 23:59:48 +0530

    started with binary search

---
+++ Binary_Search/BinarySearch.java
@@ -0,0 +1,13 @@
+package Binary_Search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        
+    }
+    
+    // Return the index
+    //return -1 if it doesnot exist
+    static int binarySearch(int[] arr, int target) {
+         
+    }
+}
