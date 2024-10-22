b206d7e3ef50b2b164f0a1ba5154011197f61cdb
shubham <jagtapshubhamsham@gmail.com>
2024-10-22 15:54:03 +0530

    started building logics

---
+++ LeetCode/ProductArray.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+// Given an integer array nums, return an array answer such that answer[i] is equal to
+// the product of all the elements of nums except nums[i].
+
+public class ProductArray {
+    public static void main(String[] args) {
+        
+    }   
+    
+    static int[] productExceptSelf(int[] nums) {
+
+    }
+}
+
+
