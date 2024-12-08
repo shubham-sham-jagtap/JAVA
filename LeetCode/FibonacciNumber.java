aaffdc62681aec7226f0fbf7b285320e034eaeee
shubham <jagtapshubhamsham@gmail.com>
2024-12-08 23:48:37 +0530

    done

--- LeetCode/FibonacciNumber.java
+++ LeetCode/FibonacciNumber.java
@@ -7,7 +7,8 @@ public class FibonacciNumber {
         System.out.println(ans);
     }
     public static int fib(int n) {
-        if(n == 0) {return 0;
+        if(n == 0) {
+            return 0;
         }
         if(n == 1){return 1;}
 
