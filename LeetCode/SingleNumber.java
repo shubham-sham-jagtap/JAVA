38259a3ebd34009d5d0110cf0e725e282b91c44b
shubham <jagtapshubhamsham@gmail.com>
2024-12-14 22:52:58 +0530

    started with 136. Single Number

---
+++ LeetCode/SingleNumber.java
@@ -0,0 +1,11 @@
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] arr = {4,1,2,1,2};
+        int ans = singleNumber(arr);
+        System.out.println(ans);
+    }
+
+    public static int singleNumber(int[] nums) {
+        
+    }
+}
