7afa42817eba665571f9f1c283a5c5ca0acff4ae
shubham <jagtapshubhamsham@gmail.com>
2024-12-15 01:08:54 +0530

    136. Single Number

--- LeetCode/SingleNumber.java
+++ LeetCode/SingleNumber.java
@@ -1,11 +1,15 @@
 public class SingleNumber {
     public static void main(String[] args) {
-        int[] arr = {4,1,2,1,2};
+        int[] arr = {1,3,2,1,2};
         int ans = singleNumber(arr);
         System.out.println(ans);
     }
 
     public static int singleNumber(int[] nums) {
-        
+        int allXOR = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            allXOR = allXOR ^ nums[i];
+        }
+        return allXOR;
     }
 }
