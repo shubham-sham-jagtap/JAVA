cf240ba9d045dfaf82e6b419fd435b94abe0e26f
shubham <jagtapshubhamsham@gmail.com>
2024-12-17 22:42:53 +0530

    blank

--- LeetCode/SortColors.java
+++ LeetCode/SortColors.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class SortColors {
     public static void main(String[] args) {
-        
+        int[] arr = {2,0,2,1,1,0};
+        int[] ans = sortColors(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[] sortColors(int[] nums){
+        int start = 0, mid = 0, end = nums.length - 1;
+
+        while (mid <= end) {
+            if(nums[start] = 0)
+        }
     }
 }
