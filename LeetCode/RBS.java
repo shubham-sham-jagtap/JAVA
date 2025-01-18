8002c8b6dedab9f0bf73280538e95f2fb6da80cf
shubham <jagtapshubhamsham@gmail.com>
2025-01-18 18:09:20 +0530

    --Time Limit Exceeded--

--- LeetCode/RBS.java
+++ LeetCode/RBS.java
@@ -2,8 +2,23 @@ package LeetCode;
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 public class RBS {
     public static void main(String[] args) {
-        int[] arr = {0,1,2, 3, 4, 5};
-        System.out.println(findPivot(arr));
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(arr, target));
+
+    }
+    public static int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        if (pivot == -1) {
+            return (binarySearch(nums, target, 0, nums.length - 1));
+        } else if (nums[pivot] == target) {
+            return (pivot);
+        } else if (target < nums[0]) {
+            return (binarySearch(nums, target, pivot + 1, nums.length - 1));
+        } else if (target >= nums[0]) {
+            return (binarySearch(nums, target, 0, pivot - 1));
+        }
+        return - 1;
     }
 
     public static int findPivot(int[] nums) {
@@ -24,4 +39,17 @@ public class RBS {
         }
         return -1;
     }
+     public static int binarySearch(int[] nums, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if(target < nums[mid]) {
+                end = mid - 1;
+            } else if(target > nums[mid]) {
+                start = mid + 1;
+            } else if ( target == nums[mid]) {
+                return mid;
+            }
+        }
+        return -1;
+     }
 }
