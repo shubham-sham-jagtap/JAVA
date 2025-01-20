4aa3620fe2081003cff09e376b38be9f44c440d4
shubham <jagtapshubhamsham@gmail.com>
2025-01-20 16:24:25 +0530

    Rotated Binary Search

--- LeetCode/RBS.java
+++ LeetCode/RBS.java
@@ -29,11 +29,11 @@ public class RBS {
             //4 cases
             if(mid < end &&  nums[mid] > nums[mid + 1]) {
                 return mid;
-            } else if(mid > start && nums[mid] < nums[mid - 1]) {
+            } if(mid > start && nums[mid] < nums[mid - 1]) {
                 return mid - 1;
-            } else if (nums[start] > nums[mid]) {
+            } if (nums[start] > nums[mid]) {
                 end = mid - 1;
-            } else if (nums[start] < nums[mid]) {
+            } else {
                 start = mid + 1;
             }
         }
