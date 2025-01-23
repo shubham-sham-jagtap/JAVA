43162d5912f2881a2ddd52543b612922831fed91
shubham <jagtapshubhamsham@gmail.com>
2025-01-23 15:52:31 +0530

    a proper approch

--- Sorting/Cyclic Sort/Cyclic.java
+++ Sorting/Cyclic Sort/Cyclic.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 class Cyclic {
   public static void main(String[] args) {
     int[] arr = {5, 4, 3, 2, 1};
-    cyclic(arr);
+    sort(arr);
     System.out.println(Arrays.toString(arr));
   }
+  // My logic 
   public static void cyclic(int[] nums) {
     for(int i = 1; i <= nums.length; i++) {
       if(nums[i-1] != i) {
@@ -14,5 +15,22 @@ class Cyclic {
         nums[i - 1] = temp;
       }
     }
+  }
+  // Kunal's Logic 
+  public static void sort(int[] nums) {
+    int i = 0;
+    while (i < nums.length) {
+      int correct = nums[i] - 1;
+      if(nums[i] != nums[correct]){
+        swap(nums, i, correct);
+      } else {
+        i++;
+      }
+    }
+  }
+  public static void swap(int[] nums, int first, int second) {
+    int temp = nums[first];
+    nums[first] = nums[second];
+    nums[second] = temp;
   }
 }
