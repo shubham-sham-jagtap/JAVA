be7ecf5c90578e56a27d5f5e7c0f48fb7207e841
shubham <jagtapshubhamsham@gmail.com>
2025-01-24 13:11:39 +0530

    Cyclic sort approch to find missing number

--- LeetCode/MissingNumber.java
+++ LeetCode/MissingNumber.java
@@ -1,10 +1,12 @@
 public class MissingNumber {
+//https://leetcode.com/problems/missing-number/description/
     public static void main(String[] args) {
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 9};
-        int ans = missingNumber(arr);
+        int ans = missingNumberCyclic(arr);
         System.out.println(ans);
     }
 
+    //XOR approch
     public static int missingNumber(int[] nums) {
         int allXOR = 0;
         for(int i = 0; i <= nums.length; i++) {
@@ -16,5 +18,26 @@ public class MissingNumber {
         }
 
         return allXOR;
+    }
+
+    //Cyclic Sort approch
+    public static int missingNumberCyclic(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i];
+            if(nums[i] < nums.length && nums[correct] != nums[i]) {
+                int temp = nums[correct];
+                nums[correct] = nums[i];
+                nums[i] = temp;
+            }
+            i++;
+        }
+
+        for(int index = 0; index < nums.length; index++) {
+            if(nums[index] != index) {
+                return index;
+            }
+        }
+        return nums.length;
     }
 }
