4f82dc57af8455a5a22ba2497bb2a4a18939ab05
shubham <jagtapshubhamsham@gmail.com>
2025-02-02 16:44:43 +0530

    48. Rotate Image

--- LeetCode/RotateImage.java
+++ LeetCode/RotateImage.java
@@ -5,23 +5,26 @@ import java.util.Arrays;
 public class RotateImage {
   public static void main(String[] args) {
     int[][] matrix = {
-      {1, 2, 3},
-      {4, 5, 6},
-      {7, 8, 9}
+      {5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}
     };
     rotate(matrix);
   }
   public static void rotate(int[][] matrix) {
       int start = 0, end = matrix.length - 1;
       while(start < end) {
-        for(int i = start; i <= end; i++) {
-          int temp = matrix[start][i];
-          matrix[start][i] = matrix[end][i];
-          matrix[end][i] = temp;
-        }
-        start++;
-        end--;
+        int[] temp = matrix[start];
+            matrix[start] = matrix[end];
+            matrix[end] = temp;
+            start++;
+            end--;
       }  
+      for(int row = 0; row < matrix.length; row++) {
+        for(int cols = row + 1; cols < matrix.length; cols++) {
+          int temp = matrix[row][cols];
+          matrix[row][cols] = matrix[cols][row];
+          matrix[cols][row] = temp;
+        }
+      }
       System.out.println(Arrays.deepToString(matrix));
   }
 }
