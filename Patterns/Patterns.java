b4f65abda33cbde6de015e23d5c9c8aada8cd936
shubham <jagtapshubhamsham@gmail.com>
2025-02-08 13:18:56 +0530

    a numeric diamon pattern

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -2,7 +2,7 @@ public class Patterns {
   public static void main(String[] args) {
     // pattern1(4);
     // pattern2(4);
-    pattern30(5);
+    pattern17(5);
   }
    
   static void pattern30(int n) {
@@ -18,7 +18,23 @@ public class Patterns {
         System.out.print(cols+" ");
       }
       System.out.println();
+    }
   }
+  static void pattern17(int n) {
+    for(int row = 1; row < 2*n; row++) {
+      int c = row > n ? 2 * n - row : row;
+      for(int s = 1; s <= n- c; s++) {
+        System.out.print("  ");
+      }
+      for(int col = c; col >= 1; col--) { 
+        System.out.print(" "+ col);
+      }
+      for(int col = 2; col <= c; col++) {
+        System.out.print(" "+col);
+      }
+      System.out.println();
+    }
+    
   }
   static void pattern28(int n) {
     for(int row = 1; row <= 2*n - 1 ; row++) {
